9446e1169aeb9e6b116ee7a8559a66b1ce6fd24e
Filipemtb <filipera3@gmail.com>
2024-02-29 12:48:59 -0300

    create: add validations

--- desafio matriz/src/application/Main.java
+++ desafio matriz/src/application/Main.java
@@ -18,27 +18,33 @@ public class Main {
         System.out.println("Input the numbers that will be alocated in your array: ");
         for (int i = 0; i<matriz.length; i++) {
             for (int j = 0; j< matriz[i].length; j++) {
-
                 matriz[i][j] = scanner.nextInt();
             }
         }
 
         System.out.print("Input the number into the array you want to check: ");
-        int chosenNumberToAvaliated = scanner.nextInt();
+        int chosenNumberToValidated = scanner.nextInt();
 
         for (int i = 0; i<matriz.length; i++) {
-            for (int j = 0; j<matriz.length; j++) {
-                if (matriz[i][j] == chosenNumberToAvaliated) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                if (matriz[i][j] == chosenNumberToValidated) {
                     System.out.println("Position: " + i +", "+ j);
-                    System.out.println("Left: " + matriz[i][j-1]);
-                    System.out.println("Right: " + matriz[i][j+1]);
-                    System.out.println("Up: " + matriz[i-1][j]);
-                    System.out.println("Down: " + matriz[i+1][j]);
+                    if (j - 1 >= 0) {
+                        System.out.println("Left: " + matriz[i][j-1]);
+                    }
+                    if (j + 1 < matriz[i].length) {
+                        System.out.println("Right: " + matriz[i][j+1]);
+                    }
+                    if (i-1 >= 0) {
+                        System.out.println("Up: " + matriz[i-1][j]);
+                    }
+                    if (i < matriz.length - 1) {
+                        System.out.println("Down: " + matriz[i+1][j]);
+                    }
                 }
             }
+            System.out.println();
         }
-
         scanner.close();
-
     }
 }
